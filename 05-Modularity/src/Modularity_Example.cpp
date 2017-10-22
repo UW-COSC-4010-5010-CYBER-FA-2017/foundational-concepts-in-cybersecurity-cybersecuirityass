4307c037a235ab87c75a8671f46c69be0ddd9ef5
BerhaneTsegay <31521112+BerhaneTsegay@users.noreply.github.com>
2017-10-22 12:47:35 -0600

    Modularity.cpp

--- 05-Modularity/src/Modularity_Example.cpp
+++ 05-Modularity/src/Modularity_Example.cpp
@@ -1,40 +1,38 @@
-//
-//
-//
-//#include <iostream>
-//#include <conio.h>
-//using namespace std;
-//
-//float getInput(void);
-//float convert(float f);
-//void display(float c);
-//
-//int main()
-//{
-//   float fahrenheit;
-//   float celsius;
-//   fahrenheit = getInput();
-//   celsius = convert(fahrenheit);
-//   display(celsius);
-//   return 0;
-//}
-//
-//float getInput()
-//{
-//float input;
-//cout << "Enter Temperature in Degree Fahrenheit" << endl;
-//cin >> input;
-//return input;
-//}
-//
-//float convert(float f)
-//{
-//float c;
-//c = (f-32.0)*(5.0/9.0);
-//return c;
-//}
-//
-//void display(float c)
-//{
-//   cout << "The Temperature in Celsius is: " << c << endl;
-//}
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+float getInput(void);
+float convert(float f);
+void display(float c);
+
+int main()
+{
+   float fahrenheit;
+   float celsius;
+   fahrenheit = getInput();
+   celsius = convert(fahrenheit);
+   display(celsius);
+   return 0;
+}
+
+float getInput()
+{
+float input;
+cout << "Enter Temperature in Degree Fahrenheit" << endl;
+cin >> input;
+return input;
+}
+
+float convert(float f)
+{
+float c;
+c = (f-32.0)*(5.0/9.0);
+return c;
+}
+
+void display(float c)
+{
+   cout << "The Temperature in Celsius is: " << c << endl;
+}
