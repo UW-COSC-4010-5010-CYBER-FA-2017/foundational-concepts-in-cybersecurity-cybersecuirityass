18b94d88a935bf9dd114c6ed8731e529ae9b73eb
BerhaneTsegay <31521112+BerhaneTsegay@users.noreply.github.com>
2017-10-30 23:39:22 -0600

    Minimization Done

---
+++ 10-Minimization/src/Minimization_Example.cpp
@@ -0,0 +1,18 @@
+/* Berhane Tsegay
+Cyber Security
+Minimization Good Example
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  float x;             //variable to find ln of it
+   //======User Input=====
+   cout << "Enter the number you want to find its natural logarithm:" << endl;
+   cin >> x;
+   //Get result using library function log(x)
+   cout << "ln("<< x << ") using C++ function is= " << log(x) << endl;
+   }
---
+++ 10-Minimization/src/Minimization_NonExample.cpp
@@ -0,0 +1,62 @@
+
+/* Berhane Tsegay
+Cyber Security
+Minimization bad Example
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+float ComputelogXcomplex(float x,int precision);
+
+
+int main()
+{
+
+   int precision;     //The the highest order of polynomial for precision.
+   float x;             //variable to find ln of it using Tayler series.
+   float logx =0.0, lnx= 0.0;
+   //======User Input=====
+   cout << "Enter the order of Precision:" << endl;
+   cin >> precision;
+   cout << "Enter a number b/w 0 and 2 to find its natural logarithm:" << endl;
+   cin >> x;
+check:
+    if(x>0&& x<=2)
+{
+  //Get actual value using library function log(x)
+   cout << "ln("<< x << ") using C++ function is= " << log(x) << endl;
+   // Get the result using taylor series
+   logx = ComputelogXcomplex(x, precision);
+   cout << "\n(Complex) The value of ln(" << x << ") using tayler series of order "<< precision<< " is: "<< logx <<endl;
+
+}
+else {
+    cout << "Please Enter a floating point number between 0 and 2" << endl;
+     cin >> x;
+     goto check;
+}
+
+   }
+// function to find lnx in a more complex way
+float ComputelogXcomplex(float x,int precision)
+{
+
+ float logX;
+	float Diff=0.0;;
+	float OneRaisedToN;
+	float Temp;
+   OneRaisedToN = 1.0;
+   Diff = Temp = x - 1;
+   logX = OneRaisedToN*Diff;
+
+		for (int n = 2;n <= precision;n++)
+		{
+			OneRaisedToN = -OneRaisedToN;
+			Diff *= Temp;
+			logX += (OneRaisedToN / n)*Diff;
+      //logX+= (pow(-1,n+1)*pow((x-1),n))/n;
+		}
+		return logX;
+	}
